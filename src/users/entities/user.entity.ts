57d074fb8fd772efbfdc4668f7eed5689c04af33
Brayan Muzzi <bcmuzzi@hotmail.com>
2025-01-11 22:35:52 -0300

    UserEntity - constructor unit tests

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,12 @@
-export class User {}
+export type UserProps = {
+  name: string
+  email: string
+  password: string
+  createAt?: Date
+}
+
+export class UserEntity {
+  constructor(public readonly props: UserProps) {
+    this.props.createAt = this.props.createAt ?? new Date()
+  }
+}
