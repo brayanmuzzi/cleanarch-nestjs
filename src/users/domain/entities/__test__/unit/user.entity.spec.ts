02fd58be937a440f8eff17810e3b54ae44e2bf0b
Brayan Muzzi <bcmuzzi@hotmail.com>
2025-01-12 18:55:51 -0300

    Adding validator fields interface

--- src/users/domain/entities/__test__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__test__/unit/user.entity.spec.ts
@@ -16,26 +16,48 @@ describe('UserEntity unit tests', () => {
     expect(sut.props.createdAt).toBeInstanceOf(Date)
   })
 
-  it('Method getter name field test', () => {
+  it('Method getter name field', () => {
     expect(sut.props.name).toBeDefined()
     expect(sut.props.name).toEqual(props.name)
     expect(typeof sut.props.name).toBe('string')
   })
 
-  it('Method getter email field test', () => {
+  it('Method setter name field', () => {
+    sut['name'] = 'other name'
+    expect(sut.props.name).toEqual('other name')
+    expect(typeof sut.props.name).toBe('string')
+  })
+
+  it('Method getter email field', () => {
     expect(sut.props.email).toBeDefined()
     expect(sut.props.email).toEqual(props.email)
     expect(typeof sut.props.email).toBe('string')
   })
 
-  it('Method getter password field test', () => {
+  it('Method getter password field', () => {
     expect(sut.props.password).toBeDefined()
     expect(sut.props.password).toEqual(props.password)
+    expect(typeof sut.props.password).toBe('string')
+  })
+
+  it('Method setter password field', () => {
+    sut['password'] = 'other password'
+    expect(sut.props.password).toEqual('other password')
     expect(typeof sut.props.password).toBe('string')
   })
 
   it('Method getter createdAt', () => {
     expect(sut.props.createdAt).toBeDefined()
     expect(sut.props.createdAt).toBeInstanceOf(Date)
+  })
+
+  it('Should update a user', () => {
+    sut.update('other name')
+    expect(sut.props.name).toEqual('other name')
+  })
+
+  it('Should update the password', () => {
+    sut.updatePassword('other password')
+    expect(sut.props.password).toEqual('other password')
   })
 })
