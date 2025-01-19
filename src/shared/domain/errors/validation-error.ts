e053800b16fc3e40b50146f824f8ac3155acb744
Brayan Muzzi <bcmuzzi@hotmail.com>
2025-01-18 23:27:40 -0300

    Update: UserEntity validate method using EntityValidationError

--- src/shared/domain/errors/validation-error.ts
+++ src/shared/domain/errors/validation-error.ts
@@ -1,5 +1,7 @@
 import { FieldsErrors } from '../validators/validator-fields.interface'
+
 export class ValidationError extends Error {}
+
 export class EntityValidationError extends Error {
   constructor(public error: FieldsErrors) {
     super('Entity Validation Error')
