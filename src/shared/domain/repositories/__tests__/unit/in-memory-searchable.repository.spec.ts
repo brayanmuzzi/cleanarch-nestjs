7835bb14ff9bdfbece30d4e049f872e909d9707a
Brayan Muzzi <bcmuzzi@hotmail.com>
2025-02-04 23:19:06 -0300

    Adding: applySort method unit test

--- src/shared/domain/repositories/__tests__/unit/in-memory-searchable.repository.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/in-memory-searchable.repository.spec.ts
@@ -47,6 +47,7 @@ describe('InMemoryRepository unit tests', () => {
         new StubEntity({ name: 'TEST', price: 50 }),
         new StubEntity({ name: 'fake', price: 50 }),
       ]
+
       const spyFilterMethod = jest.spyOn(items, 'filter')
       let itemsFiltered = await sut['applyFilter'](items, 'TEST')
       expect(itemsFiltered).toStrictEqual([items[0], items[1]])
@@ -59,6 +60,29 @@ describe('InMemoryRepository unit tests', () => {
       itemsFiltered = await sut['applyFilter'](items, 'no-filter')
       expect(itemsFiltered).toHaveLength(0)
       expect(spyFilterMethod).toHaveBeenCalledTimes(3)
+    })
+    describe('applySort method', () => {
+      it('should no sort items', async () => {
+        const items = [
+          new StubEntity({ name: 'b', price: 50 }),
+          new StubEntity({ name: 'a', price: 50 }),
+        ]
+        let itemsSorted = await sut['applySort'](items, null, null)
+        expect(itemsSorted).toStrictEqual(items)
+        itemsSorted = await sut['applySort'](items, 'price', 'asc')
+        expect(itemsSorted).toStrictEqual(items)
+      })
+      it('should sort items', async () => {
+        const items = [
+          new StubEntity({ name: 'b', price: 50 }),
+          new StubEntity({ name: 'a', price: 50 }),
+          new StubEntity({ name: 'c', price: 50 }),
+        ]
+        let itemsSorted = await sut['applySort'](items, 'name', 'asc')
+        expect(itemsSorted).toStrictEqual([items[1], items[0], items[2]])
+        itemsSorted = await sut['applySort'](items, 'name', 'desc')
+        expect(itemsSorted).toStrictEqual([items[2], items[0], items[1]])
+      })
     })
   })
 })
