b53eec1296e924671abc3d105a74b084b43de621
Brayan Muzzi <bcmuzzi@hotmail.com>
2025-03-12 21:30:48 -0300

    Refact: DeleteUserUseCase unit test

--- src/users/application/usecases/__tests__/unit/delete-user.usecase.spec.ts
+++ src/users/application/usecases/__tests__/unit/delete-user.usecase.spec.ts
@@ -1,18 +1,32 @@
-import { UserRepository } from '@/users/domain/repositories/user.repository'
-import { UseCase as DefaultUseCase } from './../../use-case'
+import { UserInMemoryRepository } from '@/users/infrastructure/database/in-memory/repositories/user-in-memory.repository'
+import { NotFoundError } from '@/shared/domain/errors/not-found-error'
+import { UserEntity } from '@/users/domain/entities/user.entity'
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder'
+import { DeleteUserUseCase } from '../../delete-user.usecase'
 
-export namespace DeleteUserUseCase {
-  export type Input = {
-    id: string
-  }
+describe('DeleteUserUseCase unit tests', () => {
+  let sut: DeleteUserUseCase.UseCase
+  let repository: UserInMemoryRepository
 
-  export type Output = void
+  beforeEach(() => {
+    repository = new UserInMemoryRepository()
+    sut = new DeleteUserUseCase.UseCase(repository)
+  })
 
-  export class UseCase implements DefaultUseCase<Input, Output> {
-    constructor(private userRepository: UserRepository.Repository) {}
+  it('Should throws error when entity not found', async () => {
+    await expect(() => sut.execute({ id: 'fakeId' })).rejects.toThrow(
+      new NotFoundError('Entity not found'),
+    )
+  })
 
-    async execute(input: Input): Promise<Output> {
-      await this.userRepository.delete(input.id)
-    }
-  }
-}
+  it('Should delete a user', async () => {
+    const spyDelete = jest.spyOn(repository, 'delete')
+    const items = [new UserEntity(UserDataBuilder({}))]
+    repository.items = items
+
+    expect(repository.items).toHaveLength(1)
+    await sut.execute({ id: items[0]._id })
+    expect(spyDelete).toHaveBeenCalledTimes(1)
+    expect(repository.items).toHaveLength(0)
+  })
+})
